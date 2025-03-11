ce84e79bc9dd7aad3d655fcb5507bcc27c4a0aae
Leonardo Albano <leonardoalbanom@yahoo.com.br>
2025-03-11 04:43:06 -0300

    config

--- src/UndefinedCRM.Api/obj/Debug/net8.0/UndefinedCRM.Api.AssemblyInfo.cs
+++ src/UndefinedCRM.Api/obj/Debug/net8.0/UndefinedCRM.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UndefinedCRM.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+294b5d798def4eca3d49c0ca9ffca321a8d5b7d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1a47246e2f095eb32791994420e620c61d6bfe2")]
 [assembly: System.Reflection.AssemblyProductAttribute("UndefinedCRM.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UndefinedCRM.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
